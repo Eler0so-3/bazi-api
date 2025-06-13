8249b241ad00eb9baaba58e0080bfc36c34f0bb9
Eler0so-3 <fomenkoegor856@gmail.com>
2025-06-13 21:00:15 +0300

    Update get_forecast.js

--- api/get_forecast.js
+++ api/get_forecast.js
@@ -1,1 +1,9 @@
+import predictions from '../predictions.json';
 
+export default function handler(req, res) {
+  const { element, yin_yang, month } = req.body || {};
+  const forecast =
+    predictions?.[yin_yang?.toLowerCase()]?.[element?.toLowerCase()]?.[month?.toString()] || "Для этой комбинации пока нет прогноза.";
+
+  res.status(200).json({ forecast });
+}
